6c1a169a1cd39d5e72bd9efc73393a6740160f04
ChernousovOEP-3quarter <75395635+ChernousovOEP-3quarter@users.noreply.github.com>
2020-12-22 14:24:03 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+ 
+class Train
+{
+  public:
+  int train_num;
+  string destination;
+  string dest_time;
+};
+ 
+int main() {   
+    int i = 0;
+    Train list[5];
+    
+    while (i < 5) {        
+        cout << "Please, enter information for train number " << i + 1 << endl;
+        cout << "Destination of train: ";
+        cin >> list[i].destination;
+        list[i].train_num = i + 1;
+        cout << "Time of departure: ";
+        cin >> list[i].dest_time;        
+        cout << endl;
+        i = i + 1;
+    }
+    
+    cout << "Please, enter number of necessary train: ";
+    int num = 0;
+    bool correct_number = false;
+    i = 0;
+    cin >> num;
+    cout << endl;
+    
+    while (i < 5) {
+      if (list[i].train_num == num) {
+        cout << "Train number"<< i + 1 <<" departuring to " << list[i].destination << " in " << list[i].dest_time;
+        correct_number = true;
+      }
+      i = i + 1;
+    }
+
+    if (correct_number == false) {
+     cout << "Error, wrong number" << endl;
+     exit(0);
+    }    
+}
